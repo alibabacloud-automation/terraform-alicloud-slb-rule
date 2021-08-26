d61d100cdbd93684db532b240975b9705556e2b3
young <zhijiang.nzj@alibaba-inc.com>
2021-08-26 16:36:57 +0800

    Removed example

--- example/main.tf
+++
@@ -1,8 +0,0 @@
-module "rule" {
-  source              = "../"
-  name                = "slb-rule-basic-config"
-  domain              = "*.test.com"
-  cookie              = "cookie-test"
-  health_check_uri    = "/test"
-  health_check_domain = "test"
-}
